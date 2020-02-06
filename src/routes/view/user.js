8ed6b14acdafd0158ef23d86ad4d28b9fabe8928
edison <lwjkkbb@sina.com>
2020-02-06 13:45:37 +0800

    fix:判断已经登录下的页面状态（isLogin）

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,12 +5,32 @@
 
 const router = require('koa-router')()
 
+/**
+ * 获取登录信息
+ * @param {Object} ctx 
+ */
+function getLogininfo(ctx){
+    let data={
+        // 默认未登录
+        isLogin:false
+    }
+    const userInfo=ctx.session.userInfo
+    if(userInfo){
+        data={
+            isLogin:true,
+            userName:userInfo.userName
+        }
+    }
+
+    return data
+}
+
 router.get('/login', async(ctx, next)=>{
-    await ctx.render('login',{})
+    await ctx.render('login',getLogininfo(ctx))
 })
 
 router.get('/register', async(ctx, next)=>{
-    await ctx.render('register',{})
+    await ctx.render('register',getLogininfo(ctx))
 })
 
 
