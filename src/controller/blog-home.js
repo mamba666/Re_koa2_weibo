8424799bcf0f36c9a7054d93f1cd765981527e97
edison <lwjkkbb@sina.com>
2020-02-07 11:23:42 +0800

    feat：增加预防xss&微博格式校验

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -3,6 +3,7 @@
  * @author edison
  */
 
+const xss=require("xss")
 const {createBlog}=require("../services/blog")
 const {SuccessModel,ErrorModel}=require("../model/ResModel")
 const {createBlogFailInfo}=require("../model/ErrorInfo")
@@ -11,9 +12,11 @@ const {createBlogFailInfo}=require("../model/ErrorInfo")
  * 创建微博
  * @param {Object} param0 userId,content,image
  */
+
 async function create({userId,content,image}){
     try{
-        const blog =await createBlog({userId,content,image})
+        // content:xss(content)预防xss攻击
+        const blog =await createBlog({userId,content:xss(content),image})
         return new SuccessModel(blog)
     }catch(ex){
         console.error(ex.message,ex.stack)
---
+++ src/validator/blog.js
@@ -0,0 +1,31 @@
+/**
+ * @description 未必数据格式校验
+ * @author edison
+ */
+
+
+const validate = require('./_validate')
+
+// 校验规则
+const SCHEMA = {
+    type: 'object',
+    properties:{
+        content:{
+            type:"string"
+        },
+        image:{
+            type:"string",
+            maxLength:255
+        }
+    }
+}
+
+/**
+ * 校验用户数据格式
+ * @param {Object} data 用户数据
+ */
+function blogValidate(data = {}) {
+    return validate(SCHEMA, data)
+}
+
+module.exports = blogValidate
