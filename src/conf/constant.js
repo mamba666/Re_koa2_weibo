525d95c97caf847171dbaf9b93d8b0838ed53a82
edison <lwjkkbb@sina.com>
2020-02-08 17:09:36 +0800

    feat:实现at功能

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -5,5 +5,8 @@
 
 
 module.exports = {
-    DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs'
+    DEFAULT_PICTURE: 'chrome-search://ntpicon/?size=48@2.000000x&url=https://www.npmjs.com/',
+
+    // 正则表达式，匹配 '@昵称 - userName'
+    REG_FOR_AT_WHO: /@(.+?)\s-\s(\w+?)\b/g
 }
--- src/services/_format.js
+++ src/services/_format.js
@@ -3,8 +3,8 @@
  * @author edison
  */
 
-const { DEFAULT_PICTURE } = require('../conf/constant')
-const {timeFormat}=require("../utils/dt")
+const { DEFAULT_PICTURE, REG_FOR_AT_WHO } = require('../conf/constant')
+const { timeFormat } = require('../utils/dt')
 
 /**
  * 用户默认头像
@@ -45,10 +45,29 @@ function _formatDBTime(obj) {
     return obj
 }
 
+/**
+ * 格式化微博内容
+ * @param {Object} obj 微博数据对象
+ */
+function _formatContent(obj) {
+    obj.contentFormat = obj.content
+
+    // 格式化 @
+    // from '哈喽 @张三 - zhangsan 你好'
+    // to '哈喽 <a href="/profile/zhangsan">张三</a> 你好'
+    obj.contentFormat = obj.contentFormat.replace(
+        REG_FOR_AT_WHO,
+        (matchStr, nickName, userName) => {
+            return `<a href="/profile/${userName}">@${nickName}</a>`
+        }
+    )
+
+    return obj
+}
 
 /**
  * 格式化微博信息
- * @param {Array|Object} list 微博列表,或者单个微博对象
+ * @param {Array|Object} list 微博列表或者单个微博对象
  */
 function formatBlog(list) {
     if (list == null) {
@@ -57,10 +76,13 @@ function formatBlog(list) {
 
     if (list instanceof Array) {
         // 数组
-        return list.map(_formatDBTime)
+        return list.map(_formatDBTime).map(_formatContent)
     }
     // 对象
-    return _formatDBTime(list)
+    let result = list
+    result = _formatDBTime(result)
+    result = _formatContent(result)
+    return result
 }
 
 module.exports = {
